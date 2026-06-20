88cbb168bc28c3414a3e9b12d92bd7eb1d562236
ADMIN <luisgcv@users.noreply.github.com>
2026-06-20 17:41:43 -0600

    Error sin log, prueba 1

--- sistema_pensiones/settings.py
+++ sistema_pensiones/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = 'RENDER' not in os.environ
-
+#DEBUG = 'RENDER' not in os.environ
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
