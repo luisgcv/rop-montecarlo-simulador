435f29571c7df024d76f820f38550b63cf185001
ADMIN <luisgcv@users.noreply.github.com>
2026-06-20 18:19:01 -0600

    Errores y mas errores

--- sistema_pensiones/wsgi.py
+++ sistema_pensiones/wsgi.py
@@ -1,14 +1,4 @@
-"""
-WSGI config for sistema_pensiones project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 import matplotlib
 matplotlib.use('Agg')
 
